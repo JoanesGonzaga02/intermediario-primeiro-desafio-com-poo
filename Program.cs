5cc9452c1098ca25e155598d549618e8fed5ba4f
joanesgonzaga02 <joanesgonzaga.dev02@gmail.com>
2023-12-01 12:48:22 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Pessoa
+{  
+  private string nome;
+  private int idade;
+    public Pessoa(string n, int i)
+    {
+        nome = n;
+        idade = i;
+    }
+    public string GetNome()
+    {
+        return nome;
+    }
+    public int GetIdade()
+    {
+        return idade;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+       string n = Console.ReadLine();
+       int idade = int.Parse(Console.ReadLine());
+       Pessoa p1 = new Pessoa(n, idade);      
+       Console.WriteLine($"Nome: {n}, Idade: {idade}");
+    }
+}
